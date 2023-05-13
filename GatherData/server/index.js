584ef0126c0bd32a0ce1e5a556a5b0b37bd07242
ApostolosFotopoulos <apfotopoulos1997@gmail.com>
2023-05-13 17:37:41 +0300

    initialized react frontend

--- GatherData/server/index.js
+++ GatherData/server/index.js
@@ -9,10 +9,37 @@ app.use(express.json());
 app.post("/patient", async (req, res) => {
   try {
     console.log(req.body);
-    const { name: patientName } = req.body;
+    const { patientName } = req.body;
     const newPatient = await pool.query("INSERT INTO patient (name) VALUES ($1) RETURNING *", [patientName]);
     console.log(newPatient.rows[0]);
     res.json(newPatient.rows[0]);
+  } catch (err) {}
+});
+
+app.get("/patient/:id", async (req, res) => {
+  try {
+    const { id: patientId } = req.params;
+    const patient = await pool.query("SELECT * FROM patient WHERE patient_id = $1", [patientId]);
+    console.log(patient.rows[0]);
+    res.json(patient.rows[0]);
+  } catch (err) {}
+});
+
+app.put("/patient/:id", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { patientName } = req.body;
+    const { id: patientId } = req.params;
+    const patient = await pool.query("UPDATE patient SET name=$1 WHERE patient_id = $2", [patientName, patientId]);
+    res.json("update success");
+  } catch (err) {}
+});
+
+app.delete("/patient/:id", async (req, res) => {
+  try {
+    const { id: patientId } = req.params;
+    const patient = await pool.query("DELETE FROM patient WHERE patient_id = $1", [patientId]);
+    res.json("delete success");
   } catch (err) {}
 });
 
