2be58d7f5c99ef94a94afa2002c235834547d972
ApostolosFotopoulos <apfotopoulos1997@gmail.com>
2023-07-11 17:35:12 +0300

    changed graph settings

--- GatherData/client/src/pages/dashboard/sessionHistory/SessionHistory.js
+++ GatherData/client/src/pages/dashboard/sessionHistory/SessionHistory.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { LineChart, ResponsiveContainer, Line, XAxis, YAxis, CartesianGrid } from "recharts";
+import {
+  LineChart,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Line,
+  XAxis,
+  YAxis,
+  Label,
+  CartesianGrid,
+} from "recharts";
 
 // Utils
 import { hasSelectedPatient } from "utils/validateData";
@@ -31,6 +41,11 @@ export default function SessionHistory() {
         let res = await getLatestSessions({ patientId: selectedPatient.patientId }).unwrap();
         res = [...res].reverse();
         console.log(res);
+        res = res.map((ses, i) => ({
+          ...ses,
+          xLabel: i + 1,
+        }));
+        console.log(res);
         setSessions(res);
       } catch (err) {
         console.log(err);
@@ -53,8 +68,20 @@ export default function SessionHistory() {
         <ResponsiveContainer width="95%" height={300}>
           <LineChart data={sessions}>
             <CartesianGrid strokeDasharray="5 5" stroke="white" />
-            <XAxis stroke="white" />
-            <YAxis type="number" domain={[0, 1]} stroke="white" />
+            <XAxis dataKey="xLabel" stroke="white">
+              <Label value="Recent Sessions" position="insideTop" offset={36} />
+            </XAxis>
+            <YAxis type="number" domain={[0, 1]} stroke="white">
+              <Label
+                value="Results"
+                position="insideLeft"
+                offset={6}
+                style={{ textAnchor: "middle" }}
+                angle={270}
+              />
+            </YAxis>
+            <Tooltip contentStyle={{ backgroundColor: "rgb(69, 69, 69)" }} />
+            <Legend align="right" />
             <Line type="monotone" dataKey="normal" stroke="blue" />
             <Line type="monotone" dataKey="cp1" stroke="yellow" />
             <Line type="monotone" dataKey="cp2" stroke="red" />
@@ -66,9 +93,6 @@ export default function SessionHistory() {
             <CartesianGrid strokeDasharray="5 5" stroke="white" />
             <XAxis stroke="white" />
             <YAxis type="number" domain={[0, 1]} stroke="white" />
-            <Line type="monotone" dataKey="normal" stroke="blue" />
-            <Line type="monotone" dataKey="cp1" stroke="yellow" />
-            <Line type="monotone" dataKey="cp2" stroke="red" />
           </LineChart>
         </ResponsiveContainer>
       )}
